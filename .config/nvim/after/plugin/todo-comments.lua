fffd78128c4ec83a8065affe066a7b4e19853ea8
David Hain <d.hain@gmx.at>
2023-07-13 10:28:35 +0200

    feat: neovim todo-comments && link-visitor

---
+++ .config/nvim/after/plugin/todo-comments.lua
@@ -0,0 +1,18 @@
+require("todo-comments").setup {
+    keywords = {
+        PERF = {
+            color = "#ba9bf7",
+        },
+        NOTE = {
+            color = "#10b880",
+        },
+    }
+}
+-- TODO:
+-- FIX:
+-- FIXME:
+-- HACK:
+-- WARN:
+-- PERF:
+-- NOTE:
+-- TEST:
--- .config/nvim/lua/dhain/packer.lua
+++ .config/nvim/lua/dhain/packer.lua
@@ -7,22 +7,34 @@ return require("packer").startup(function(use)
     use "navarasu/onedark.nvim"
     use {
         "nvim-treesitter/nvim-treesitter",
-        { run = ":TSUpdate" }
+        {
+            run = ":TSUpdate"
+        },
     }
     use {
         "brenoprata10/nvim-highlight-colors",
         config = {
             require("nvim-highlight-colors").setup()
-        }
+        },
+    }
+    use {
+        "folke/todo-comments.nvim",
+        requires = "nvim-lua/plenary.nvim",
+        -- TODO:
+        -- FIX:
+        -- HACK:
+        -- WARN:
+        -- PERF:
+        -- NOTE:
+        -- TEST:
+        -- FIXME: sam ihna
     }
 
     -- Better than netrw
     use "nvim-tree/nvim-web-devicons"
     use {
         "nvim-tree/nvim-tree.lua",
-        requires = {
-            "nvim-tree/nvim-web-devicons"
-        }
+        requires = "nvim-tree/nvim-web-devicons",
     }
 
     -- Lualine
@@ -31,14 +43,15 @@ return require("packer").startup(function(use)
         requires = {
             "kyazdani42/nvim-web-devicons",
             opt = true
-        }
+        },
     }
 
     -- Blazingly fast navigation
     use "theprimeagen/harpoon"
     use {
-        "nvim-telescope/telescope.nvim", tag = "0.1.1",
-        requires = { { "nvim-lua/plenary.nvim" } }
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.1",
+        requires = "nvim-lua/plenary.nvim",
     }
 
     -- Git things
@@ -53,32 +66,34 @@ return require("packer").startup(function(use)
     use {
         "kylechui/nvim-surround",
         tag = "*",
-        config = function()
+        config = {
             require("nvim-surround").setup()
-        end
+        },
     }
 
-    -- Useful things
-    use "mbbill/undotree"
+    -- QoL
     use {
         "windwp/nvim-autopairs",
         config = {
             require("nvim-autopairs").setup()
-        }
+        },
     }
     use "alvan/vim-closetag"
     use "nvim-treesitter/nvim-treesitter-context"
-    use  {
+
+    -- Useful things
+    use "mbbill/undotree"
+    use {
         "nvim-treesitter/playground",
-        requires = { "nvim-treesitter" },
+        requires = "nvim-treesitter",
     }
     -- Expanding and collapsing code
     use {
         "Wansmer/treesj",
-        requires = { "nvim-treesitter" },
-        config = function()
+        requires = "nvim-treesitter",
+        config = {
             require("treesj").setup()
-        end,
+        },
     }
     use "xiyaowong/link-visitor.nvim"
     -- Documentation generator
@@ -113,7 +128,7 @@ return require("packer").startup(function(use)
             -- Snippets
             { "L3MON4D3/LuaSnip" }, -- Required
             { "rafamadriz/friendly-snippets" },
-        }
+        },
     }
     use "elkowar/yuck.vim"
 
