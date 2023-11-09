3d332a7ba63b7041cb02348a14a761eaa23b42a8
David Hain <d.hain@gmx.at>
2023-11-09 19:15:15 +0100

    feat: added wezterm config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,81 @@
+local wezterm = require("wezterm");
+
+local config = {}
+
+-- Clearer error messages in newer versions
+if wezterm.config_builder then
+    config = wezterm.config_builder()
+end
+
+-- Config
+config.color_scheme = "One Dark (Gogh)"
+config.font = wezterm.font("JetBrains Mono")
+config.use_fancy_tab_bar = false
+
+config.keys = {
+    {
+        mods = "CTRL",
+        key = "w",
+        action = wezterm.action.CloseCurrentTab { confirm = true },
+    },
+    {
+        mods = "CTRL",
+        key = "t",
+        action = wezterm.action { SpawnTab = "CurrentPaneDomain" },
+    },
+
+    -- Tabs
+    {
+        mods = "CTRL",
+        key = "1",
+        action = wezterm.action { ActivateTab = 0 },
+    },
+    {
+        mods = "CTRL",
+        key = "2",
+        action = wezterm.action { ActivateTab = 1 },
+    },
+    {
+        mods = "CTRL",
+        key = "3",
+        action = wezterm.action { ActivateTab = 2 },
+    },
+    {
+        mods = "CTRL",
+        key = "4",
+        action = wezterm.action { ActivateTab = 3 },
+    },
+    {
+        mods = "CTRL",
+        key = "5",
+        action = wezterm.action { ActivateTab = 4 },
+    },
+    {
+        mods = "CTRL",
+        key = "6",
+        action = wezterm.action { ActivateTab = 5 },
+    },
+    {
+        mods = "CTRL",
+        key = "7",
+        action = wezterm.action { ActivateTab = 6 },
+    },
+    {
+        mods = "CTRL",
+        key = "8",
+        action = wezterm.action { ActivateTab = 7 },
+    },
+    {
+        mods = "CTRL",
+        key = "9",
+        action = wezterm.action { ActivateTab = 8 },
+    },
+    {
+        mods = "CTRL",
+        key = "0",
+        action = wezterm.action { ActivateTab = 9 },
+    },
+
+}
+
+return config
