249bd735987a1df14a12c89a95b37172cb6c16b8
David Hain <d.hain@gmx.at>
2024-06-09 00:18:10 +0200

    feat: remove workspace keybinds

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -13,7 +13,6 @@ config.check_for_updates = false
 -- Config
 config.color_scheme = "One Dark (Gogh)"
 config.font = wezterm.font("JetBrains Mono")
--- config.font = wezterm.font("Monocraft")
 config.use_fancy_tab_bar = false
 config.window_close_confirmation = "NeverPrompt"
 config.force_reverse_video_cursor = true
@@ -22,95 +21,102 @@ config.colors = {
     foreground = "#abb2bf",
 }
 
+local action = wezterm.action
+
 config.mouse_bindings = {
     -- Font Size
     -- Scrolling up while holding CTRL increases the font size
     {
+        mods = "CTRL",
         event = { Down = { streak = 1, button = { WheelUp = 1 } } },
-        mods = 'CTRL',
-        action = wezterm.action.IncreaseFontSize,
+        action = action.IncreaseFontSize,
     },
 
     -- Scrolling down while holding CTRL decreases the font size
     {
+        mods = "CTRL",
         event = { Down = { streak = 1, button = { WheelDown = 1 } } },
-        mods = 'CTRL',
-        action = wezterm.action.DecreaseFontSize,
+        action = action.DecreaseFontSize,
     },
 }
 
 config.keys = {
+    -- ----- Open/Move/Close Tabs -----
+    -- Close current tab
     {
         mods = "CTRL|SHIFT",
         key = "w",
-        action = wezterm.action.CloseCurrentTab { confirm = true },
+        action = action.CloseCurrentTab { confirm = true },
     },
+    -- Open new tab
     {
         mods = "CTRL",
         key = "t",
-        action = wezterm.action { SpawnTab = "CurrentPaneDomain" },
+        action = action { SpawnTab = "CurrentPaneDomain" },
     },
+    -- Move current tab to the left
     {
         mods = "CTRL|SHIFT",
         key = "h",
-        action = wezterm.action { MoveTabRelative = -1 },
+        action = action { MoveTabRelative = -1 },
     },
+    -- Move current tab to the right
     {
         mods = "CTRL|SHIFT",
         key = "l",
-        action = wezterm.action { MoveTabRelative = 1 },
+        action = action { MoveTabRelative = 1 },
     },
 
-    -- Tabs
+    -- ----- Changing Tabs -----
     {
         mods = "CTRL",
         key = "1",
-        action = wezterm.action { ActivateTab = 0 },
+        action = action { ActivateTab = 0 },
     },
     {
         mods = "CTRL",
         key = "2",
-        action = wezterm.action { ActivateTab = 1 },
+        action = action { ActivateTab = 1 },
     },
     {
         mods = "CTRL",
         key = "3",
-        action = wezterm.action { ActivateTab = 2 },
+        action = action { ActivateTab = 2 },
     },
     {
         mods = "CTRL",
         key = "4",
-        action = wezterm.action { ActivateTab = 3 },
+        action = action { ActivateTab = 3 },
     },
     {
         mods = "CTRL",
         key = "5",
-        action = wezterm.action { ActivateTab = 4 },
+        action = action { ActivateTab = 4 },
     },
     {
         mods = "CTRL",
         key = "6",
-        action = wezterm.action { ActivateTab = 5 },
+        action = action { ActivateTab = 5 },
     },
     {
         mods = "CTRL",
         key = "7",
-        action = wezterm.action { ActivateTab = 6 },
+        action = action { ActivateTab = 6 },
     },
     {
         mods = "CTRL",
         key = "8",
-        action = wezterm.action { ActivateTab = 7 },
+        action = action { ActivateTab = 7 },
     },
     {
         mods = "CTRL",
         key = "9",
-        action = wezterm.action { ActivateTab = 8 },
+        action = action { ActivateTab = 8 },
     },
     {
         mods = "CTRL",
         key = "0",
-        action = wezterm.action { ActivateTab = 9 },
+        action = action { ActivateTab = 9 },
     },
 
 }
