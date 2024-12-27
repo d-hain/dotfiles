e134d8de228b902f4ea862380c171d71c6e0b983
David Hain <d.hain@gmx.at>
2024-12-27 22:38:40 +0100

    feat: nixos: starting and stopping hyprsunset automatically using systemd-timers ?! (not completely tested if the timers work)

--- nixos/hosts/default/configuration.nix
+++ nixos/hosts/default/configuration.nix
@@ -152,6 +152,7 @@ in {
       waybar
       flameshot
       (pkgs-unstable.hyprshot)
+      hyprsunset
       pavucontrol
       # Bluetooth GUI (doesn't work but makes it work) see:
       # https://github.com/bluez/bluez/issues/673#issuecomment-1849132576
@@ -220,6 +221,41 @@ in {
   ################
   ### Programs ###
   ################
+
+  # Start Hyprsunset at 18:00:00 every day and turn it off at 8:00:00
+  systemd.user.services.hyprsunset = {
+      enable = true;
+      wantedBy = ["default.target"];
+      description = "Starts Hyprsunset";
+      serviceConfig = {
+          Type = "simple";
+          ExecStart = "${pkgs.hyprsunset}/bin/hyprsunset -t 5555";
+      };
+  };
+  systemd.user.timers.hyprsunset-evening = {
+      wantedBy = ["timers.target"];
+      after = ["hyprsunset-morning.service"];
+      timerConfig = {
+          OnCalendar = "*-*-* 18:00:00";
+          Persistent = true;
+          Unit = "hyprsunset.service";
+      };
+  };
+  systemd.user.services.hyprsunset-off = {
+      description = "Stops 'hyprsunset.service'";
+      serviceConfig = {
+          Type = "oneshot";
+          ExecStart = "systemctl --user stop hyprsunset.service";
+      };
+  };
+  systemd.user.timers.hyprsunset-morning = {
+      wantedBy = ["timers.target"];
+      timerConfig = {
+          OnCalendar = "*-*-* 8:00:00";
+          Persistent = true;
+          Unit = "hyprsunset-off.service";
+      };
+  };
 
   # Greeter
   services.greetd = {
