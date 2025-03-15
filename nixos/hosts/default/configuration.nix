87f247f596a9434d17170853b6e0521d428fae3d
David Hain <d.hain@gmx.at>
2025-03-15 20:58:57 +0100

    feat: nixos: enable udisks2 for automatic USB mounting

--- nixos/hosts/default/configuration.nix
+++ nixos/hosts/default/configuration.nix
@@ -418,6 +418,9 @@ in {
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
+  # Automount USB devices
+  services.udisks2.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   programs.gnupg.agent = {
